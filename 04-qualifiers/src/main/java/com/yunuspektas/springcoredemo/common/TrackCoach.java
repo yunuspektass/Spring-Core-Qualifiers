0ccfbcc973293df67bdfdf68207925bddb9a4966
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-02-24 14:12:17 +0300

    Spring Core Qualifiers
    04-qualifier

---
+++ 04-qualifiers/src/main/java/com/yunuspektas/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.yunuspektas.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements  Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+}
